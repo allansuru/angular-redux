20bb74d272aedfb314460cba909222772b5e5842
Allan SuRu <https://github.com/allansuru/ProjetoBase.git>
2018-03-05 20:57:04 -0300

    add projeto

---
+++ src/app/todo-dashboard/todo-dashboard.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { NgRedux, select } from 'ng2-redux'; 
+import { IAppState } from '../store'; 
+import { CLEAR_TODOS } from '../actions'; 
+
+@Component({
+  selector: 'app-todo-dashboard',
+  templateUrl: './todo-dashboard.component.html',
+  styleUrls: ['./todo-dashboard.component.css']
+})
+export class TodoDashboardComponent {
+  @select() todos; 
+  @select() lastUpdate; 
+  @select() itemDeletado;
+  @select() itemAdicionado
+  
+  constructor(private ngRedux: NgRedux<IAppState>) {
+  }
+
+  clearTodos() {
+    this.ngRedux.dispatch({ type: CLEAR_TODOS });
+  }
+}
